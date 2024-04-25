629cccc4cfbedff9cf0341539ea15c93e7934fd4
Chris <129601042+Operations-Department@users.noreply.github.com>
2024-04-24 19:24:08 -0500

    include console logs and outputs of function when called

--- assignment1/recursive-fibonacci.js
+++ assignment1/recursive-fibonacci.js
@@ -6,8 +6,14 @@ function fibsRec(n) {
     let array = fibsRec(n-1);
 
     array.push(array[array.length-1] + array[array.length-2]); 
-    
+
     return array;
 }
 
-console.log(fibsRec(5));
+console.log(fibsRec('hello'));     //Output: []
+console.log(fibsRec(-1));          //Output: []
+console.log(fibsRec(0));           //Output: []
+console.log(fibsRec(1));           //Output: [ 0 ]
+console.log(fibsRec(2));           //Output: [ 0, 1 ]
+console.log(fibsRec(5));           //Output: [ 0, 1, 1, 2, 3 ]
+console.log(fibsRec(8));           //Output: [ 0, 1, 1, 2, 3, 5, 8, 13 ]
